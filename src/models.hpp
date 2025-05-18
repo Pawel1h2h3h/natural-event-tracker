767b3db52b94e65438be4ff366630df891838f73
paweljaa <pawel16092004@gmail.com>
2025-05-18 16:05:49 +0200

    Forgot to add models - simple struckts helping Class Api and Event.

---
+++ src/models.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <optional>
+#include <cpr/cpr.h>
+
+struct ApiRequest {
+    cpr::Url url;
+    cpr::Parameters params;
+    cpr::Header headers;
+};
+
+
+struct CategoryS {
+    std::string id;
+    std::string title;
+};
+
+struct Source {
+    std::string id;
+    std::string url;
+};
+
+struct Geometry {
+    std::string date;
+    std::string type;
+    std::vector<double> coordinates;
+    std::optional<std::string> magnitudeUnit;
+    std::optional<double> magnitudeValue;
+};
